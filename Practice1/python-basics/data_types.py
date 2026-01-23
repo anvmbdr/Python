cda910acfbea335caa4ee85e8908acf6635a5e32
Aiym <aiym.bbbbb@gmail.com>
2026-01-23 22:36:35 +0500

    Add dt examples

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,27 @@
+#1
+x = 5
+print(type(x))
+
+#2
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+#3
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+#4
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+#5
+x = True
+print(type(x))
+
+#6
+x = b"Hello"
+print(type(x))
+
+
+
+
