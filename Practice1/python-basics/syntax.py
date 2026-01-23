c285f2c500dd55406cf8335fa59a87c036663d62
Aiym <aiym.bbbbb@gmail.com>
2026-01-23 21:50:49 +0500

    Add syntax examples

---
+++ Practice1/python-basics/syntax.py
@@ -0,0 +1,19 @@
+#1
+if 5 > 2:
+  print("Five is greater than two!")
+
+#2
+if 6 > 3:
+  print("Six is greater than three!")
+
+#3
+if 10 > 2:
+  print("Ten is greater than two!")
+
+#4
+if 10 != 2:
+  print("Ten is not equal to two!")
+
+#5
+if 7 > 1:
+  print("Seven is greater than one!")
