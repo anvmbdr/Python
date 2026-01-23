ce8063d15c958ae798dabcc6fb6bb9019cfe3a66
Aiym <aiym.bbbbb@gmail.com>
2026-01-23 22:55:22 +0500

    Add string examples

---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,28 @@
+#1
+a = "Hello, World!"
+print(a[1]) #output: e
+
+#2
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a) #output: just no quotes
+
+#3
+for x in "banana":
+  print(x) #as column
+
+#4
+a = "Hello, World!"
+print(len(a)) #13
+
+#5
+txt = "The best things in life are free!"
+print("free" in txt) #True
+
+#6
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
