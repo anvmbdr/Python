cf3762f824ac3c4b685f8bf3d3c40446db3584e5
Aiym <aiym.bbbbb@gmail.com>
2026-01-27 17:02:41 +0500

    Update modify.py

--- Practice1/python-basics/modify.py
+++ Practice1/python-basics/modify.py
@@ -2,11 +2,11 @@ a = "Hello, World!"
 print(a.upper())
 
 a = " Hello, World! "
-print(a.strip()) # returns "Hello, World!" #from the beginning or from the end
+print(a.strip()) 
 
 a = "Hello, World!"
 print(a.replace("H", "J"))
 
 a = "Hello, World!"
-print(a.split(",")) # returns ['Hello', ' World!']
+print(a.split(",")) 
 
